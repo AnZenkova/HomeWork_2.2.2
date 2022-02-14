a86c423f3474aacd4780d789ca7139c830a0060a
AnZenkova <nastyonka16@mail.ru>
2022-02-14 21:50:32 +1000

    Первый блок

---
+++ src/com/company/Bicycle.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Bicycle {
+    private String modelName;
+    private int wheelsCount;
+
+    public Bicycle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+
+    public static void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Bicycle bicycle1 = new Bicycle("Sport-1.1", 2);
+        Bicycle.updateTyre();
     }
 }
